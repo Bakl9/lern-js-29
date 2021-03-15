60ab1c79e5b259f134cc1e173d1098bef08ae6e0
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-03-15 15:25:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -151,3 +151,58 @@ let user = {
 // }
 
 
+    ///Задачи >>
+
+// let user = {
+//     name: "John",
+//     surname: "Smith",
+
+//     name: "Pete"
+// }
+
+// delete  user.name
+
+// alert (user.name)
+
+//     function isEmpty(obj) {
+//         for (let key in obj) {
+//             return true
+//         }
+//         return false
+//     }
+
+
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130 
+// }
+
+// alert ("John" + "Ann" + "Pete" )
+
+
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130,
+
+// };
+
+// let sum = 0 
+// for (let key in salaries) {
+//     sum += salaries[key];
+// }
+
+// alert(sum)
+
+
+// function multiplayNumeric(obj) {
+//     for (let key in obj) {
+//         if (typeof obj [key]) {
+//             obj[key] *= 2;
+//         }
+//     }
+// }
+
