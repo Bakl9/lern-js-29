2b055d1280c012a7c5916982b1bd51857870b93e
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-03-15 12:28:39 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,91 @@
+// let user = {
+//     name: "Johan",
+//     age: 40,
+// }
+
+// user .  Isadmin = true
+
+
+//  alert(user.Isadmin)
+
+
+
+// let user = {
+//     name: "Makarov",
+//     age: 26,
+// }
+
+//  user . Hobby = "bike"
+
+// alert(user.Hobby)
+
+
+// let user = {}
+//     user ["like birds"] = true
+//       alert (user["like birds"]) 
+//     delete user ["like birds"] 
+
+// let user = {
+//     name: "John",
+//     age: 30 
+// };
+
+// let key = prompt('Что вы хотите узнать о пользователе?',"name");
+
+// alert (user[key])
+
+
+// let fruit = prompt ('Какой фрукт купить ?','apple');
+
+// let bag = {
+//     [fruit]: 5,
+// };
+
+// alert( bag.apple);
+
+
+// let fruit = prompt ('какой фрукт можно выбрать ?', 'apple');
+// let bag = {};
+
+// bag [fruit] = 5
+
+
+
+
+
+// function makeUser(name, age) {
+//     return {
+//         name:name,
+//         ahe: age,
+//     };
+// }
+
+// let user = makeUser('John', 30);
+// alert(user.name);
+
+// let obj = { 
+//     let: 1,
+//     for: 3,
+//     return: 4,
+// }
+
+// alert (obj.let + obj.for + obj.return)
+
+// let obj = {
+//     0:"Тест"
+// };
+
+// alert (obj['0'])
+// alert (obj[0])
+
+// let obj = {};
+// obj.__proto__ = 5;
+// alert(obj.__proto__)
+
+// let user = {
+//     name: "john",
+//     age: 30,
+// }
+
+// alert (user.noSuchProperty === undefined)
+
